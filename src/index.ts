ed3cf7cc8149517daa5df088fb47173b0f2f519f
Ros McMahon <rosmcmahon@gmail.com>
2021-04-13 13:07:11 +0100

    tensorflow: processing speed enhancement
    - ensure model only gets loaded once
    - todo: check tf compilation on linux
    - todo: process results

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config() //first line of entrypoint
 import col from 'ansi-colors'
 import { logger } from './utils/logger'
 import { scanner } from './scanner/poller'
-import { checkImage, checkImageTxid } from './rating/image-rating'
+import { NsfwTools } from './rating/image-rating'
 
 
 /* start http server */
@@ -14,7 +14,6 @@ const main = async()=> {
 	try {
 		// scanner() //do not await
 		
-
 		
 		const txids: string[] = require('../tests/image-txids').default
 		console.log(txids.length)
@@ -23,7 +22,9 @@ const main = async()=> {
 		// 	await checkImageTxid(txid)
 		// }
 
-		await Promise.all(txids.map(txid => checkImageTxid(txid)))
+		await NsfwTools.loadModel()
+		
+		await Promise.all(txids.map(txid => NsfwTools.checkImageTxid(txid)))
 
 
 		
--- src/rating/image-rating.ts
+++ src/rating/image-rating.ts
@@ -4,41 +4,58 @@ import axios from 'axios'
 import * as tf from '@tensorflow/tfjs-node'
 import * as nsfw from 'nsfwjs'
 
-let _model: nsfw.NSFWJS
-const cachedModel = async()=> {
-	if(_model){
-		return _model
+//static everything to keep that model
+export class NsfwTools {
+	static _model: nsfw.NSFWJS
+	private constructor(){} //hide
+
+	static async loadModel()   {
+
+		if(NsfwTools._model){
+			// model already loaded
+			return NsfwTools._model
+		}
+
+		logger('loading model once')
+
+		NsfwTools._model = await nsfw.load('file://src/model/', {size: 299})
+		return NsfwTools._model
 	}
-	return await nsfw.load('file://src/model/', {size: 299}) 
-}
 
-export const checkImage = async(pic: Buffer)=> {
+	static checkImage = async(pic: Buffer)=> {
 
-	const model = await cachedModel()
-
-	const image = tf.node.decodeImage(pic,3) as tf.Tensor3D
+		const model = await NsfwTools.loadModel()
 	
-	const predictions = await model.classify(image)
-	image.dispose() // explicit TensorFlow memory management
-
-	return predictions
-}
-
-
-export const checkImageTxid = async(txid: string)=> {
-	try {
-
-		const url = `https://arweave.net/${txid}`
-		const pic = await axios.get(url, {
-			responseType: 'arraybuffer',
-		})
+		const image = tf.node.decodeImage(pic,3) as tf.Tensor3D
 		
-		const res = await checkImage(pic.data)
-
-		console.log(url, res)
-
-		
-	} catch (e) {
-		logger('Error checking', txid, e.name, ':', e.message)
+		const predictions = await model.classify(image)
+		image.dispose() // explicit TensorFlow memory management
+	
+		return predictions
 	}
+
+	static checkImageUrl = async(url: string)=> {
+		try {
+			const pic = await axios.get(url, {
+				responseType: 'arraybuffer',
+			})
+			
+			const res = await NsfwTools.checkImage(pic.data)
+	
+			console.log(url, res)
+	
+			//TODO: process results
+
+		} catch (e) {
+			logger('Error checking', url, e.name, ':', e.message)
+		}
+	}
+
+	static checkImageTxid = async(txid: string)=> NsfwTools.checkImageUrl(`https://arweave.net/${txid}`)
+
 }
+
+
+
+
+
