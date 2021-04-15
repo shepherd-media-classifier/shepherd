12b7447297ef1346b9ed8f93b86959e6df35e39d
Ros McMahon <rosmcmahon@gmail.com>
2021-04-15 17:28:32 +0100

    server: implement list caching (5mins timeout)

--- src/server/blacklist.ts
+++ src/server/blacklist.ts
@@ -5,12 +5,39 @@ import { logger } from '../utils/logger'
 const db = getDb()
 const prefix = 'server'
 
+//serve cache for 5 mins
+const timeout = 5 * 60 * 1000 // min 5 mins between list updates
+let _lastBlack = 0
+let _lastWhite = 0
+let _whiteText = ''
+let _blackText = ''
+
 export const getBlacklist = async(black: boolean)=> {
+
+	const now = new Date().valueOf()
+	if(black){
+		if(now - _lastBlack > timeout) _lastBlack = now
+		else{ 
+			logger(prefix, 'serving cached blacklist')
+			return _blackText	
+		}
+	} else{
+		if(now - _lastWhite > timeout) _lastWhite = now
+		else{ 
+			logger(prefix, 'serving cached whitelist')
+			return _whiteText 
+		}
+	}
+
 	const records = await db<TxRecord>('txs').where({flagged: black})
 	logger(prefix, 'flagged txs retrieved', records.length)
 	let text = ''
 	for (const record of records) {
 		text += record.txid + '\n'
 	}
+
+	if(black) _blackText = text
+	else _whiteText = text
+	
 	return text
 }
