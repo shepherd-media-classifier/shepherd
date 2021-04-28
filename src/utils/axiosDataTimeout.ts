caea5e9481a752e80b444f129d55ef2f833bd60b
Ros McMahon <rosmcmahon@gmail.com>
2021-04-28 21:56:28 +0100

    rater: undo those timeout changes for now:
    - the streaming to buffer works in most cases, but has introduced a new
    waiting/hanging connection scenario in some outlier case, presumable
    needs to be identified & handled manually. Not worth the effort for now.

--- src/utils/axiosDataTimeout.ts
+++ src/utils/axiosDataTimeout.ts
@@ -3,73 +3,73 @@ import { NO_DATA_TIMEOUT } from "../constants"
 import { logger } from "./logger"
 
 
-// export const axiosDataTimeout = async(url: string)=> {
-	
-// 		/**
-// 		 * Axios never times out if the connection is opened correctly with non-zero Content-Length, but no data is ever returned.
-// 		 * The workaround is to set a timeout, cancel the request, and throw an error.
-// 		 */
-// 		 const source = axios.CancelToken.source()
-// 		 const timer = setTimeout( ()=>source.cancel(), NO_DATA_TIMEOUT )
- 
-// 		 try{
- 
-// 			 const res = await axios.get(url, {
-// 				 cancelToken: source.token,
-// 				 responseType: 'arraybuffer',
-// 				 onDownloadProgress: ()=> clearTimeout(timer),
-// 			 })
-// 			 clearTimeout(timer)
- 
-// 			 return res
- 
-// 		 }catch(e){
-// 			 clearTimeout(timer)
-// 			 if(e.response || e.code){
-// 				 throw(e)
-// 			 }
-// 			 throw new Error(`Timeout of ${NO_DATA_TIMEOUT}ms exceeded`)
-// 		 }
-// }
-
 export const axiosDataTimeout = async(url: string)=> {
-	return new Promise<Buffer>( async(resolve, reject) => {
-		
-		const source = axios.CancelToken.source()
 	
-		const timer = setTimeout( ()=>{
-			source.cancel()
-			reject(new Error(`Timeout of ${NO_DATA_TIMEOUT}ms exceeded`))
-		}, NO_DATA_TIMEOUT )
-	
-		try{
-			
-			const { data: stream } = await axios.get(url, {
-				cancelToken: source.token,
-				responseType: 'stream',
-			})
-			
-			let buffers: Uint8Array[] = []
-	
-			stream.on('data', (buffer: Uint8Array)=>{
-				clearTimeout(timer)
-				buffers.push(buffer)
-			})
-	
-			stream.on('end', ()=>{
-				const data = Buffer.concat(buffers)
-				resolve(data)
-				return;
-			})
-
-			stream.on('error', (e: any)=>{
-				logger('READ STREAM ERROR')
-				reject(e)
-			})
-	
-		}catch(e){
-			clearTimeout(timer)
-			reject(e)
-		}
-	})
+		/**
+		 * Axios never times out if the connection is opened correctly with non-zero Content-Length, but no data is ever returned.
+		 * The workaround is to set a timeout, cancel the request, and throw an error.
+		 */
+		 const source = axios.CancelToken.source()
+		 const timer = setTimeout( ()=>source.cancel(), NO_DATA_TIMEOUT )
+ 
+		 try{
+ 
+			 const res = await axios.get(url, {
+				 cancelToken: source.token,
+				 responseType: 'arraybuffer',
+				//  onDownloadProgress: ()=> clearTimeout(timer),
+			 })
+			 clearTimeout(timer)
+ 
+			 return res.data
+ 
+		 }catch(e){
+			 clearTimeout(timer)
+			 if(e.response || e.code){
+				 throw(e)
+			 }
+			 throw new Error(`Timeout of ${NO_DATA_TIMEOUT}ms exceeded`)
+		 }
 }
+
+// export const axiosDataTimeout = async(url: string)=> {
+// 	return new Promise<Buffer>( async(resolve, reject) => {
+		
+// 		const source = axios.CancelToken.source()
+	
+// 		const timer = setTimeout( ()=>{
+// 			source.cancel()
+// 			reject(new Error(`Timeout of ${NO_DATA_TIMEOUT}ms exceeded`))
+// 		}, NO_DATA_TIMEOUT )
+	
+// 		try{
+			
+// 			const { data: stream } = await axios.get(url, {
+// 				cancelToken: source.token,
+// 				responseType: 'stream',
+// 			})
+			
+// 			let buffers: Uint8Array[] = []
+	
+// 			stream.on('data', (buffer: Uint8Array)=>{
+// 				clearTimeout(timer)
+// 				buffers.push(buffer)
+// 			})
+	
+// 			stream.on('end', ()=>{
+// 				const data = Buffer.concat(buffers)
+// 				resolve(data)
+// 				return;
+// 			})
+
+// 			stream.on('error', (e: any)=>{
+// 				logger('READ STREAM ERROR')
+// 				reject(e)
+// 			})
+	
+// 		}catch(e){
+// 			clearTimeout(timer)
+// 			reject(e)
+// 		}
+// 	})
+// }
