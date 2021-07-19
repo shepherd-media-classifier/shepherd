d148c290ca4047ccdd73b9a9bf21e243eafffe66
Ros McMahon <rosmcmahon@gmail.com>
2021-07-19 18:55:41 +0700

    video: mark corrupt video data + ffmpeg cap fails
    - we can pick up corrupt data using ffprobe metainfo
    - still not getting intelligent errors when ffmpeg screencaps fail
    --will handle this at a later date, marking as corrupt-maybe

--- src/rating/video/screencaps.ts
+++ src/rating/video/screencaps.ts
@@ -4,21 +4,80 @@ import { logger } from '../../utils/logger'
 
 
 export const createScreencaps = async(txid: string)=> {
-	const folderpath = VID_TMPDIR + txid + '/'
+	try{
+		const folderpath = VID_TMPDIR + txid + '/'
 
-	const video = await new ffmpeg(folderpath + txid)
-	// logger(txid, video.metadata)
+		const video = await new ffmpeg(folderpath + txid)
+		
 
-	const duration = video.metadata.duration!.seconds
-	if(duration > 1){
-		video.setVideoStartTime(1)
+		if(process.env.NODE_ENV === 'test'){
+			console.log(txid, video.metadata)
+			// //@ts-ignore
+			// console.log(txid, video.info_configuration)
+		}
+		// video.setDisableAudio() 
+		//@ts-ignore
+		if(video.metadata.filename === ''){
+			throw new Error('corrupt video data')
+		}
+
+		const duration = video.metadata.duration!.seconds
+		// if(duration > 1){
+		// 	video.setVideoStartTime(1)
+		// }
+		const frames = await video.fnExtractFrameToJPG(folderpath,{
+			every_n_seconds: 10,
+		})
+
+		logger(txid, 'duration:', duration, 'number of screencaps:', (frames.length - 1)) //frames[0] is the video path
+
+		return frames
+	}catch(e){
+		if(process.env.NODE_ENV === 'test'){
+			logger(txid, 'error in createScreencaps', e.code)
+			// logger(txid, 'error in createScreencaps', e.message)
+		}
+		throw e
 	}
-	const frames = await video.fnExtractFrameToJPG(folderpath,{
-		every_n_seconds: 10,
-	})
-
-	logger(txid, 'duration:', duration, 'number of screencaps:', (frames.length - 1)) //frames[0] is the video path
-
-	return frames
 }
+
+// export const createScreencapsFluentFfmpeg = async(txid: string)=> {
+// 	try{
+// 		const folderpath = VID_TMPDIR + txid + '/'
+
+// 		const video = Fluent(folderpath + txid, { /*logger:*/ })//.withNoAudio().keepDisplayAspectRatio()
+		
+// 		if(process.env.NODE_ENV === 'test'){
+// 			video.on('codecData', data=>{
+// 				logger(txid, 'codecData', data.video)
+// 			})
+// 			video.ffprobe( (err, data)=> {
+// 				if(err) logger(txid, 'ffprobe error', err)
+// 				logger(txid, 'ffprobe', data)
+// 			})
+// 		}
+
+// 		// video.on('stderr', e=>{
+// 		// 	logger(txid, 'STDERR HERE:', e)
+// 		// })
+
+// 		video.on('error', e=>{
+// 			logger(txid, 'ERRORRR', e.name )
+// 			logger(txid, 'ERRORRR', e.message )
+// 		})
+
+// 		video.on('end', ()=> logger(txid, 'finished creating caps'))
+
+// 		video.screenshots({
+// 			count: 4,
+// 			folder: folderpath,
+
+// 		})
+
+// 		return frames
+// 	}catch(e){
+// 		console.log(e.name, ':', e.message)
+// 		throw e
+// 	}
+// }
 
--- tests/video-badtx.spec.ts
+++ tests/video-badtx.spec.ts
@@ -1,6 +1,6 @@
 require('dotenv').config() //first line of entrypoint
 import { expect } from 'chai'
-import { createScreencaps } from '../src/rating/video/screencaps'
+import { createScreencapsFluentFfmpeg, createScreencaps } from '../src/rating/video/screencaps'
 import { VidDownloadRecord, videoDownload } from '../src/rating/video/video-prepare'
 import col from 'ansi-colors'
 
@@ -8,16 +8,12 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('video bad tx handling tests', ()=> {
 
-	it('7mRWvWP5KPoDfmpbGYILChc9bjjXpiPhxuhXwlnODik: Invalid data found when processing input', async()=>{
+	it('7mRWvWP5KPoDfmpbGYILChc9bjjXpiPhxuhXwlnODik: ffmpeg (ffprobe) corrupt data found', async()=>{
 		//@ts-ignore
 		const badData: VidDownloadRecord = {
 			complete: 'FALSE',
 			content_size: 262144,
-			// txid: '7mRWvWP5KPoDfmpbGYILChc9bjjXpiPhxuhXwlnODik', // corrupt video data
-			txid: 'nSX3Qaz-r1NF2dJ4Xh-pMrD6VNt_5wmtu6AgezO3h9U', //good tx
-			// txid: '4OCrYht6DFl1Bw4H0wmim_Afi24ACAtYTcFHiBZJ7wY', //?bad tx?
-			// txid: '2k1GzAM4KAiDPebpZOo-F8PyP45e1NJVU-LaB5RAKy4', //?bad tx?
-			// txid: '2Q5OE8QdmneyEnOMuxTlv04Q7PFezfSDXtaFqjI93CM', //?bad tx?
+			txid: '7mRWvWP5KPoDfmpbGYILChc9bjjXpiPhxuhXwlnODik', // corrupt video data
 		}
 		try{
 			const res = await videoDownload(badData)
@@ -25,11 +21,29 @@ describe('video bad tx handling tests', ()=> {
 			if(badData.complete === 'ERROR') throw new Error(badData.txid + ' download failed')
 			//we're expecting an ffmpeg error in createScreencaps
 			const frames = await createScreencaps(badData.txid) 
-			expect(true).true
+			expect(true).false // <= we should not get here
 		}catch(e){
-			console.log(col.green( e.message))
-			console.log(e)
-			expect(true).false
+			expect(e.message).eq('corrupt video data')
+		}
+	}).timeout(0)
+
+	it('2Q5OE8QdmneyEnOMuxTlv04Q7PFezfSDXtaFqjI93CM: ffmpeg fell over creating screencaps', async()=>{
+		//@ts-ignore
+		const badData: VidDownloadRecord = {
+			complete: 'FALSE',
+			content_size: 262144,
+			// txid: 'nSX3Qaz-r1NF2dJ4Xh-pMrD6VNt_5wmtu6AgezO3h9U', //good tx
+			txid: '2Q5OE8QdmneyEnOMuxTlv04Q7PFezfSDXtaFqjI93CM', //partial file
+		}
+		try{
+			const res = await videoDownload(badData)
+			while(badData.complete === 'FALSE'){ await sleep(500) }
+			if(badData.complete === 'ERROR') throw new Error(badData.txid + ' download failed')
+			//we're expecting an ffmpeg error in createScreencaps
+			const frames = await createScreencaps(badData.txid) 
+			expect(true).false // <= we should not get here
+		}catch(e){
+			expect(e.code).eq(69)
 		}
 	}).timeout(0)
 
