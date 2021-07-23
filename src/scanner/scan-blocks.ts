b7830e1dddbb7930ab52c805ef5925e4c9148a78
Ros McMahon <rosmcmahon@gmail.com>
2021-07-23 15:09:36 +0700

    others: disable scanning for text for now

--- src/scanner/scan-blocks.ts
+++ src/scanner/scan-blocks.ts
@@ -99,7 +99,9 @@ export const scanBlocks = async (minBlock: number, maxBlock: number): Promise<IG
 		logger(prefix, `making three scans of ${((maxBlock - minBlock) + 1)} blocks, from block ${minBlock} to ${maxBlock}`)
 		const images = await getRecords(imageTypes)
 		const videos = await getRecords(videoTypes)
-		const texts = await getRecords(textTypes)
+		
+		/* not needed yet */
+		const texts: TxScanned[] = [] //await getRecords(textTypes)
 
 		await db<StateRecord>('states')
 			.where({pname: 'scanner_position'})
@@ -119,7 +121,7 @@ export const scanBlocks = async (minBlock: number, maxBlock: number): Promise<IG
 	}
 }
 
-/* This only gets called once in a blue moon */
+/* This only gets called for media found in Bundles */
 const getContentType = async (txid: string) => {
 
 	logger(prefix, 'grabbing missing Content-Type', txid)
