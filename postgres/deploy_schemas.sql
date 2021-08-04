f9acf4f586835e6c562c4195eaf2094c4549b36e
Ros McMahon <rosmcmahon@gmail.com>
2021-08-04 06:50:08 +0200

    packaging: readme + packaging

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -11,7 +11,7 @@ CREATE TABLE txs (
 	content_size INTEGER NOT NULL,
 	flagged BOOLEAN,
 	valid_data BOOLEAN,
-	data__reason TEXT,
+	data_reason TEXT,
 	nsfw_porn real,
 	nsfw_sexy real,
 	nsfw_hentai real,
