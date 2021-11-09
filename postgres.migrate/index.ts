b0c6bea665108b3511acc431276a33100e0cf917
Ros McMahon <rosmcmahon@gmail.com>
2021-11-09 18:55:39 +0700

    remove postgres.migrate image

--- postgres.migrate/index.ts
+++
@@ -1,9 +0,0 @@
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
-const loop = async()=>{
-	while(true){
-		console.log('welcome to the loop')
-		await sleep(600000)
-	}
-}
-loop();
