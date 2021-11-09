60ee9c3f92456c081d36532f1d694555f9ce27ca
Ros McMahon <rosmcmahon@gmail.com>
2021-11-10 00:04:17 +0700

    integrate migrate => scanner + convert Dockerfile to multibuild

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,28 @@
 FROM node:14 as base
 
-# turn off the nuisance update message 
+# turn off the nuisance nodejs update message 
 ARG NO_UPDATE_NOTIFIER=true
 ENV NO_UPDATE_NOTIFIER=true
 RUN npm config set update-notifier false
-
-# install ffmpeg dependency
-RUN apt update 
-RUN apt install ffmpeg -y
-
 # create app directory
 WORKDIR /app
-
-FROM base as prod
 COPY package*.json ./
 RUN npm ci --only=production
 COPY . .
-EXPOSE 80
 ENV NODE_ENV=production
+
+
+FROM base as rating
+# install ffmpeg dependency
+RUN apt update 
+RUN apt install ffmpeg -y
 ENV TF_CPP_MIN_LOG_LEVEL=2
+ENTRYPOINT node -r ts-node/register src/rating/index.ts
+
+FROM base as web
+# EXPOSE 80 # irrelevant?
 ENTRYPOINT node -r ts-node/register src/$PROCESS_NAME/index.ts
+
+FROM base as scanner
+RUN npm install -g knex ts-node
+ENTRYPOINT knex migrate:latest && node -r ts-node/register src/$PROCESS_NAME/index.ts
---
+++ migrations/20211109065721_init_migration.ts
@@ -0,0 +1,45 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('txs', (table)=>{
+		table.bigIncrements('id')
+		table.string('txid', 43).unique().notNullable()
+		table.string('content_type').notNullable()
+		table.bigInteger('content_size').notNullable()
+		table.boolean('flagged')
+		table.boolean('valid_data')
+		table.boolean('data_reason')
+
+		table.float('nsfw_porn')
+		table.float('nsfw_sexy')
+		table.float('nsfw_hentai')
+		table.float('nsfw_neutral')
+		table.float('nsfw_drawings')
+
+		table.timestamp('last_update_date', { useTz: true }).defaultTo(knex.fn.now())
+	})
+	await knex.schema.raw('ALTER TABLE txs ADD CONSTRAINT cc_txid CHECK ((char_length(txid) = 43))')
+
+	/**
+	 * Remove all that dbowner stuff
+	 */
+
+	await knex.schema.createTable('states', table=> {
+		table.increments('id')
+		table.text('pname').notNullable()
+		table.bigInteger('value').notNullable()
+	})
+	await knex('states').insert([
+		{ pname: 'scanner_position', value: 0},
+		{ pname: 'rating_position', value: 0},
+	])
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+	//this seems dangerous?
+	await knex.schema.dropTableIfExists('txs')
+	await knex.schema.dropTableIfExists('states')
+}
+
