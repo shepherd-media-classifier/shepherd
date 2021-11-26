59c3c5b8f6b9f4694ee270249d1615d124e77d7b
Ros McMahon <rosmcmahon@gmail.com>
2021-11-27 05:00:39 +0700

    bug: docker logout before login

--- ecs.sh
+++ ecs.sh
@@ -7,6 +7,7 @@ export $(egrep -v '^#' .env | xargs)
 # export IMAGE_TAG=$ts
 
 export IMAGE_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+docker logout
 aws ecr get-login-password --region $AWS_REGION | docker login --password-stdin --username AWS $IMAGE_REPO
 
 docker compose -f docker-compose.yml -f docker-compose.aws.yml build
