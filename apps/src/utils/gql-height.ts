86ad33e64b35f3ac5bacf796428e8e7921b3dab0
Ros McMahon <rosmcmahon@gmail.com>
2022-04-25 14:32:48 +0700

    patch: fast gql retry can result in infinite loop

--- apps/src/utils/gql-height.ts
+++ apps/src/utils/gql-height.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 /* use GQL height over /info height, as both can get out of sync */
 export const getGqlHeight = async () => {
@@ -9,7 +10,8 @@ export const getGqlHeight = async () => {
 			const { data } =  await axios.post('https://arweave.net/graphql', {	query })
 			return +data.data.blocks.edges[0].node.height
 		}catch(e:any){
-			console.log('error polling gql height', e.message, 'retrying..')
+			console.log('error polling gql height', e.message, 'retrying in 10s..')
+			await sleep(10000)
 		}
 	}
 }
