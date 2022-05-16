9006f1b109daddc110997e34bc80dc5132dc3bf1
Ros McMahon <rosmcmahon@gmail.com>
2022-05-16 16:57:30 +0700

    upgrade constainers from fermium to gallium

--- infra/node14-ffmpeg/build.sh
+++ infra/node14-ffmpeg/build.sh
@@ -2,5 +2,6 @@
 echo 'Warning this will change your docker build system'
 docker buildx install
 docker buildx create --use --name shepbuild
-docker buildx build --push --tag rosmcmahon/node14-ffmpeg:multi-arch --platform linux/amd64,linux/arm64 . 
-# docker buildx rm shepbuild
+docker login
+docker buildx build --push --tag rosmcmahon/node16-ffmpeg:multi-arch --platform linux/amd64,linux/arm64 . 
+docker buildx rm shepbuild
