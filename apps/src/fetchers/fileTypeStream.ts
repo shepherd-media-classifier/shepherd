890c1fafe56c6f2ba347bf120faf2754080e7ed6
Ros McMahon <rosmcmahon@gmail.com>
2022-05-25 18:25:31 +0700

    cleanup

--- apps/src/fetchers/fileTypeStream.ts
+++ apps/src/fetchers/fileTypeStream.ts
@@ -3,7 +3,7 @@ import { Readable } from 'stream';
 import { logger } from '../common/utils/logger';
 import { s3Delete } from './s3Stream';
 
-export const filetypeStream = async(incoming: Readable, txid: string)=> {
+export const filetypeStream = async(incoming: Readable, txid: string, dbMime: string)=> {
 	const ft = await fromStream(incoming)
 	if( ft && (
 			ft.mime.startsWith('image')
@@ -15,9 +15,9 @@ export const filetypeStream = async(incoming: Readable, txid: string)=> {
 		if(process.env.NODE_ENV === 'test') logger(filetypeStream.name, `${txid} tested ok ${ft.mime}`)
 		return true;
 	}else{
-		if(process.env.NODE_ENV === 'test') logger(filetypeStream.name, `${txid} tested bad ${ft?.mime}`)
+		// if(process.env.NODE_ENV === 'test') 
+		logger(filetypeStream.name, `${txid} original ${dbMime} rejected with ${ft?.mime}`)
 		incoming.emit('error', new Error('BAD_MIME'))
-		s3Delete(txid)
 		return false;
 	}
 }
--- apps/src/fetchers/s3Stream.ts
+++ apps/src/fetchers/s3Stream.ts
@@ -1,5 +1,6 @@
 import { S3 } from 'aws-sdk'
 import { Readable } from 'stream'
+import { FetchersStatus } from '../common/constants'
 import { logger } from '../common/utils/logger'
 
 const prefix = 's3stream'
@@ -26,8 +27,9 @@ export const s3Stream = async(readable: Readable, mimetype: string, txid: string
 
 	logger(prefix, 'uploading', txid, mimetype)
 
-	readable.on('error',e=>{
-		if(['NO_DATA', 'BAD_MIME'].includes(e.message)){ 
+	readable.on('error', e =>{
+		const errorStatuses: FetchersStatus[] = ['NO_DATA', 'BAD_MIME']
+		if(errorStatuses.includes(e.message)){ 
 			logger(prefix, 'aborting. error', e.message, txid)
 			uploader.abort() 
 		}else{
@@ -37,6 +39,7 @@ export const s3Stream = async(readable: Readable, mimetype: string, txid: string
 
 	let uploader: S3.ManagedUpload
 	try{
+
 		uploader = s3.upload({
 			Bucket: bucketName,
 			Key: txid,
@@ -46,7 +49,9 @@ export const s3Stream = async(readable: Readable, mimetype: string, txid: string
 		const data = await uploader.promise()
 		logger(prefix, 'uploaded buffer. Location', data.Location)
 		return 'OK';
+
 	}catch(e){
+
 		if(e instanceof Error){
 			if(e.name === 'RequestAbortedError'){
 				// not an error. we requested to abort
@@ -54,18 +59,19 @@ export const s3Stream = async(readable: Readable, mimetype: string, txid: string
 			}
 			logger(prefix, 'UNHANDLED S3 ERROR', e.name,':',e.message)
 		}
-		//just throw stuff like NoSuchBucket, UnknownEndpoint, etc. needs to be handled externally
-		throw e;	
+		//just throw errors like NoSuchBucket, UnknownEndpoint, etc. needs to be handled externally
+		throw e;
+
 	}
 }
 
 export const s3Delete = async (txid: string) => {
-	logger(prefix, `deleting ${txid} ...`)
+	logger(s3Delete.name, `deleting ${txid} ...`)
 	
 	await s3.deleteObject({
 		Bucket: bucketName,
 		Key: txid,
 	}).promise()
 	
-	logger(prefix, `sent delete command on ${txid} without throwing error`)
+	logger(s3Delete.name, `sent delete command for ${txid} without throwing error`)
 }
