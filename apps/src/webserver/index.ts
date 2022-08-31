3fa691a27270408536188ba978b62205e9e66856
Ros McMahon <rosmcmahon@gmail.com>
2022-08-31 03:37:34 +0000

    feat: add ip access control to rangelist.txt

--- apps/src/webserver/index.ts
+++ apps/src/webserver/index.ts
@@ -10,29 +10,43 @@ const port = 80
 
 // app.use(cors())
 
-console.log('process.env.BLACKLIST_ALLOWED',process.env.BLACKLIST_ALLOWED)
-const accessList: string[] = JSON.parse(process.env.BLACKLIST_ALLOWED || '[]')
-console.log(`accessList (BLACKLIST_ALLOWED) for '/blacklist.txt' access`, accessList)
+/* load the IP access lists */
+const accessBlacklist: string[] = JSON.parse(process.env.BLACKLIST_ALLOWED || '[]')
+console.log(`accessList (BLACKLIST_ALLOWED) for '/blacklist.txt' access`, accessBlacklist)
+const accessRangelist: string[] = JSON.parse(process.env.RANGELIST_ALLOWED || '[]')
+console.log(`accessList (RANGELIST_ALLOWED) for '/blacklist.txt' access`, accessRangelist)
 
 const ipAllowBlacklist = (ip: string)=>{
-	// convert from `::ffff:192.0.0.1` => `192.0.0.1`
+	/* convert from `::ffff:192.0.0.1` => `192.0.0.1` */
 	if(ip.startsWith("::ffff:")){
     ip = ip.substring(7)
 	}
-	return accessList.includes(ip)
+	return accessBlacklist.includes(ip)
 }
+const ipAllowRangelist = (ip: string)=>{
+	if(ip.startsWith("::ffff:")){
+    ip = ip.substring(7)
+	}
+	return accessRangelist.includes(ip)
+}
+
 
 
 app.get('/', async(req, res)=> {
 	res.setHeader('Content-Type', 'text/plain')
 	res.write('Webserver operational.\n\n\n')
 
+	const ip = req.headers['x-forwarded-for'] as string || 'undefined'
+	res.write(`your ip is ${ip}\n`)
 	if(process.env.BLACKLIST_ALLOWED){
-		const ip = req.headers['x-forwarded-for'] as string || 'undefined'
-		res.write(`your ip is ${ip}\n`)
 		res.write(`access blacklist: ${ipAllowBlacklist(ip)}\n`)
 	}else{
 		res.write('$BLACKLIST_ALLOWED is not defined')
+	}
+	if(process.env.RANGELIST_ALLOWED){
+		res.write(`access rangelist: ${ipAllowRangelist(ip)}\n`)
+	}else{
+		res.write('$RANGELIST_ALLOWED is not defined')
 	}
 	res.write('\n\n')
 	
@@ -43,7 +57,7 @@ app.get('/', async(req, res)=> {
 })
 
 app.get('/blacklist.txt', async(req, res)=> {
-	//if $BLACKLIST_ALLOWED not defined we let everyone access
+	/* if $BLACKLIST_ALLOWED not defined we let everyone access */
 	if(process.env.BLACKLIST_ALLOWED){
 		const ip = req.headers['x-forwarded-for'] as string || 'undefined'
 		if(!ipAllowBlacklist(ip)){
@@ -57,6 +71,14 @@ app.get('/blacklist.txt', async(req, res)=> {
 })
 
 app.get('/rangelist.txt', async(req, res)=> {
+	/* if $RANGELIST_ALLOWED not defined we let everyone access */
+	if(process.env.RANGELIST_ALLOWED){
+		const ip = req.headers['x-forwarded-for'] as string || 'undefined'
+		if(!ipAllowRangelist(ip)){
+			return res.status(403).send('403 Forbidden')
+		}
+	}
+	
 	res.setHeader('Content-Type', 'text/plain')
 	await getRangelist(res)
 	res.status(200).end()
