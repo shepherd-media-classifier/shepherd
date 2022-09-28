bcec8ae088472fab4e0570e7d8ad9176d16ba656
Ros McMahon <rosmcmahon@gmail.com>
2022-09-28 20:38:18 +0700

    fix: nsfw tests passing. `rebase back on`

--- addons/nsfw/tests.sh
+++ addons/nsfw/tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker compose -f docker-compose.test.yml build 
-docker compose -f docker-compose.test.yml run test-service npm test
-# docker compose -f docker-compose.test.yml rm -f
+docker compose -p nsfw-test -f docker-compose.test.yml build 
+docker compose -p nsfw-test -f docker-compose.test.yml run shep-runner knex migrate:latest
+docker compose -p nsfw-test -f docker-compose.test.yml run test-service npm test
+docker compose -p nsfw-test -f docker-compose.test.yml stop
+docker compose -p nsfw-test -f docker-compose.test.yml rm -f
