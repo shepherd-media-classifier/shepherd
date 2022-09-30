6da9e16bf0e84b4e38f5f498ea78d1a45074d293
Ros McMahon <rosmcmahon@gmail.com>
2022-09-30 22:07:45 +0700

    feat/plugin: fix harness + initial unit test

--- addons/nsfw/tests/harness.spec.ts
+++ addons/nsfw/tests/harness.spec.ts
@@ -1,16 +1,24 @@
 process.env['NODE_ENV'] = 'test'
 import 'mocha'
 import { expect } from 'chai'
-import { getFile } from '../src/index'
+import { getFile } from '../src/harness'
 import { S3 } from 'aws-sdk'
 import { readFileSync } from 'fs'
+import { fetch } from 'undici'
+import readline from 'readline'
 
 
 const s3 = new S3({
 	apiVersion: '2006-03-01',
-	...(process.env.SQS_LOCAL==='yes' && { endpoint: process.env.S3_LOCAL_ENDPOINT!, region: 'dummy-value' }),
-	maxRetries: 10,
+	...(process.env.SQS_LOCAL==='yes' && { 
+		endpoint: process.env.S3_LOCAL_ENDPOINT!, 
+		region: 'dummy-value',
+		s3ForcePathStyle: true, // *** needed with minio ***
+	}),
+	maxRetries: 1,
 })
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 describe(`harness tests. s3, sqs, etc`, ()=>{
 	it(`tests we can retrieve an s3 object`, async()=>{
@@ -18,21 +26,33 @@ describe(`harness tests. s3, sqs, etc`, ()=>{
 		const file = readFileSync(`${__dirname}/`+'./assets/test.png')
 		const Bucket = 'shepherd-input-mod-local'
 		const Key = 'test.png'
-		s3.upload({
-				Bucket, //this can only be run locally then
-				Key,
-				Body: file,
-			},
-			{},
-			(e, data) => {
-				if(e) throw e;
-				console.log(`data uploaded`, {data})
+		
+		while(true){
+			const { status } = await fetch(`http://s3-local:9000/minio/health/live`)
+			if(status===200){
+				console.log(`/minio/health/live ${status}`)
+				break;
+			}else{
+				console.log(`/minio/health/live ${status}. waiting 5s..`)
+				await sleep(5000)
 			}
-		)
+		}
+
+		// const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+		// console.log(`press enter to continue`)
+		// await rl[Symbol.asyncIterator]().next()
+		
+
+		const upload = await s3.upload({
+			Bucket, //this can only be run locally then
+			Key,
+			Body: file,
+		}).promise()
+		// console.log(`upload success!`)
 
 		const res = await getFile(Bucket, Key)
-		console.log({ res })
+		// console.log({ res })
 
-		expect(true).true
-	}).timeout(10000)
+		expect(res.ContentLength).eq(37978)
+	}).timeout(0)
 })
