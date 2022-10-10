e69531fd88596244b08de623bf2da639605b12f7
Ros McMahon <rosmcmahon@gmail.com>
2022-10-10 11:41:04 +0100

    task/plugin: main default error catcher

--- addons/nsfw/src/index.ts
+++ addons/nsfw/src/index.ts
@@ -1,8 +1,23 @@
-import { main } from './harness'
+import { harness } from './harness'
+import si from 'systeminformation'
+import { logger } from './utils/logger'
+import { slackLogger } from './utils/slackLogger'
 
-try{
-	main()
-	
-}catch(e){
-	
+const prefix = 'harness'
+
+const main = async()=> {
+	try{
+		harness()
+		
+	}catch(e){
+		logger(prefix, await si.mem())
+		if(e instanceof Error){
+			logger(prefix, `Unhandled error in ${main.name}!\t`, e.name, ':', e.message)
+			slackLogger(prefix, `Unhandled error in ${main.name}!\t`, e.name, ':', e.message)
+		}else{
+			logger(prefix, `Unhandled in ${main.name}!\t`, JSON.stringify(e))
+			slackLogger(prefix, `Unhandled in ${main.name}!\t`, JSON.stringify(e))
+		}
+	}	
 }
+main()
