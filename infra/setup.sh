830d8bcad9a6121f4c475142b340d9ca76379439
Ros McMahon <rosmcmahon@gmail.com>
2022-10-28 10:03:23 +0000

    chore: cleanup /infra/setup.sh

--- infra/setup.sh
+++ infra/setup.sh
@@ -79,21 +79,21 @@ sed -i '/^AWS_SECURITY_GROUP_ID/d' .env  # remove old line before adding new
 echo "AWS_SECURITY_GROUP_ID=$AWS_SECURITY_GROUP_ID" | tee -a .env
 
 export DB_HOST=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='RdsEndpointUrl'].OutputValue" \
 	--output text)
 sed -i '/^DB_HOST/d' .env  # remove old line before adding new
 echo "DB_HOST=$DB_HOST" | tee -a .env
 
 export AWS_FEEDER_QUEUE=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='SQSFeederQueue'].OutputValue" \
 	--output text)
 sed -i '/^AWS_FEEDER_QUEUE/d' .env  # remove old line before adding new
 echo "AWS_FEEDER_QUEUE=$AWS_FEEDER_QUEUE" | tee -a .env
 
 export AWS_INPUT_BUCKET=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='S3Bucket'].OutputValue" \
 	--output text)
 sed -i '/^AWS_INPUT_BUCKET/d' .env  # remove old line before adding new
@@ -107,42 +107,42 @@ sed -i '/^AWS_SQS_INPUT_QUEUE/d' .env  # remove old line before adding new
 echo "AWS_SQS_INPUT_QUEUE=$AWS_SQS_INPUT_QUEUE" | tee -a .env
 
 export SUBNET1=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='Subnet1'].OutputValue" \
 	--output text)
 sed -i '/^SUBNET1/d' .env  # remove old line before adding new
 echo "SUBNET1=$SUBNET1" | tee -a .env
 
 export SUBNET2=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='Subnet2'].OutputValue" \
 	--output text)
 sed -i '/^SUBNET2/d' .env  # remove old line before adding new
 echo "SUBNET2=$SUBNET2" | tee -a .env
 
 export SUBNET3=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='Subnet3'].OutputValue" \
 	--output text)
 sed -i '/^SUBNET3/d' .env  # remove old line before adding new
 echo "SUBNET3=$SUBNET3" | tee -a .env
 
 export ROUTETABLE=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='RouteTable'].OutputValue" \
 	--output text)
 sed -i '/^ROUTETABLE/d' .env  # remove old line before adding new
 echo "ROUTETABLE=$ROUTETABLE" | tee -a .env
 
 export ELB_ARN=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='LoadBalancerArn'].OutputValue" \
 	--output text)
 sed -i '/^ELB_ARN/d' .env  # remove old line before adding new
 echo "ELB_ARN=$ELB_ARN" | tee -a .env
 
 export ELB_DNSNAME=$(aws cloudformation describe-stacks \
-	--stack-name "shepherd-aws-stack" \
+	--stack-name shepherd-aws-stack \
 	--query "Stacks[0].Outputs[?OutputKey=='LoadBalancerDnsName'].OutputValue" \
 	--output text)
 sed -i '/^ELB_DNSNAME/d' .env  # remove old line before adding new
