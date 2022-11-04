5ec6ca68ddd589f264fe7c56c0c17fb531514e33
Ros McMahon <rosmcmahon@gmail.com>
2022-11-04 11:52:13 +0000

    feat/local: add optional extra sqs-local queues

--- start.sh
+++ start.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
+
+# exit on errors
+set -euo pipefail
+
+# import .env vars
+if [ -f ".env" ]; then
+	export $(egrep -v '^#' .env | xargs)
+	# check vars
+	echo "EXTRA_QUEUES=$EXTRA_QUEUES"
+	echo "PLUGIN=$PLUGIN"
+	if [[ -z $PLUGIN || -z $EXTRA_QUEUES ]]; then
+		echo "INFO: some environment variables not set, using defaults. check .env.example"
+	fi
+
+	# make sure .env ends in newline
+	lastchar=$(tail -c 1 .env)
+	if [ "$lastchar" != "" ]; then 
+		echo >> .env
+	fi
+else
+	echo "continuing without .env file."
+fi
+
+
 docker compose up --build -d && docker compose logs -f
