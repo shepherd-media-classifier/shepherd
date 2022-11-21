00805355e1042bc5292405d3f60fe3c264869f94
Ros McMahon <rosmcmahon@gmail.com>
2022-11-21 08:46:50 +0700

    task/checkBlocking: refactor

--- services/src/webserver/checkBlocking-functions.ts
+++ services/src/webserver/checkBlocking-functions.ts
@@ -3,8 +3,10 @@
  * - check that IPs in lists are correctly blocking data after it is flagged.
  * - also run the resource intensive rangelist dertivations so that they do not 
  * 	 build up and crash the system.
+ * 
+ * this file separates out the functions for test.
  */
-import { Readable, PassThrough } from "stream";
+import { PassThrough } from "stream";
 import readline from 'readline'
 import { logger } from "../common/shepherd-plugin-interfaces/logger";
 import { slackLogger } from "../common/utils/slackLogger";
@@ -12,11 +14,12 @@ import { slackLoggerPositive } from "../common/utils/slackLoggerPositive";
 import { getBlacklist, getRangelist } from "./blacklist";
 import { fetchRetryConnection } from "./txidToRange/fetch-retry";
 
-const INTERVAL = 1_000 * 60 * 5 // 5 minutes
+
 const prefix = 'check-blocked'
 
 
 /* load the IP access lists */
+
 // pop off first IP. this should always be a test IP
 const accessBlacklist: string[] = JSON.parse(process.env.BLACKLIST_ALLOWED || '[]')
 accessBlacklist.shift() // pop off first IP. this should always be a test IP
@@ -30,57 +33,58 @@ logger(prefix, `accessRangelist (RANGELIST_ALLOWED)`, accessRangelist)
 const gwUrls: string[] = JSON.parse(process.env.GW_URLS || '[]')
 logger(prefix, `gwUrls`, gwUrls)
 
+/* module level vars for caching */
 let _lastBlack: string[] = []
 let _lastRange: string[] = []
 
-
-const streamLists = async()=> {
+export const streamLists = async () => {
 
 	/* check all blacklist txids against GWs */
-	
-	if(gwUrls.length === 0){
+
+	if (gwUrls.length === 0) {
 		logger(prefix, `gwUrls empty`)
-	}else{
+	} else {
 		// we're reusing the server's streaming function
 		const rwBlack = new PassThrough()
-		
-		getBlacklist(rwBlack).then(()=> rwBlack.end() )
-		
+
+		getBlacklist(rwBlack).then(() => rwBlack.end())
+
 		const txids = readline.createInterface(rwBlack)
-		for await (const txid of txids){
-			console.log(`readline txid`, txid)
-	
+		for await (const txid of txids) {
+			(process.env.NODE_ENV === 'test') && console.log(`readline txid`, txid)
+
 			//TODO: be smarter later and not recheck txids confirmed as blocked
-	
-			gwUrls.forEach(gw => checkBlocked(`${gw}/${txid}`, txid) )
+
+			gwUrls.forEach(gw => checkBlocked(`${gw}/${txid}`, txid))
 		}
 		rwBlack.destroy(); txids.close();
 	}
 
 	/* check all ranges against nodes (and GWs too?) */
 
-	if(gwUrls.length === 0 && accessRangelist.length === 0){
+	if (gwUrls.length === 0 && accessRangelist.length === 0) {
 		logger(prefix, `gwUrls & accessRangelist empty`)
-	}else{
+	} else {
 		const rwRange = new PassThrough()
-		getRangelist(rwRange).then(()=> rwRange.end() )
-		
+		getRangelist(rwRange).then(() => rwRange.end())
+
 		const ranges = readline.createInterface(rwRange)
-		for await (const range of ranges){
-			console.log(`readline range`, range)
+		for await (const range of ranges) {
+			(process.env.NODE_ENV === 'test') && console.log(`readline range`, range)
+
 			const [range1, range2] = range.split(',')
-	
-			accessRangelist.forEach(ip => checkBlocked(`http://${ip}:1984/chunk/${+range1 + 1}`, range) )
+
+			accessRangelist.forEach(ip => checkBlocked(`http://${ip}:1984/chunk/${+range1 + 1}`, range))
 			gwUrls.forEach(gw => checkBlocked(`${gw}/chunk/${+range1 + 1}`, range))
 		}
 		rwRange.destroy(); ranges.close();
 	}
 }
 
-const checkBlocked = async(url: string, item: string)=> {
-	const {aborter, res:{ status } } = await fetchRetryConnection(url)
+export const checkBlocked = async (url: string, item: string) => {
+	const { aborter, res: { status } } = await fetchRetryConnection(url)
 	aborter?.abort()
-	if(status !== 404){
+	if (status !== 404) {
 		logger(prefix, `WARNING! ${item} not blocked on ${url} (status: ${status})`)
 		slackLoggerPositive('warning', `[${prefix}] ${item} not blocked on \`${url}\` (status: ${status})`)
 		return;
@@ -89,7 +93,3 @@ const checkBlocked = async(url: string, item: string)=> {
 	// slackLogger(prefix, `✅ OK. ${item} blocked on \`${url}\` (status:${status})`) //remove this noise later
 }
 
-/** main entrypoint */
-setInterval(streamLists, INTERVAL);
-/** run once at load also */
-streamLists();
--- services/src/webserver/index.ts
+++ services/src/webserver/index.ts
@@ -5,7 +5,7 @@ import { getBlacklist, getRangelist } from './blacklist'
 import { getPerfHistory, getStatsTestOnly } from './metrics'
 import si from 'systeminformation'
 import './perf-cron' //starts automatically
-import './checkBlocking-calcRanges' //starts automatically
+import './checkBlocking-timer' //starts automatically
 
 const prefix = 'webserver'
 const app = express()
@@ -19,51 +19,51 @@ logger(prefix, `accessList (BLACKLIST_ALLOWED) for '/blacklist.txt' access`, acc
 const accessRangelist: string[] = JSON.parse(process.env.RANGELIST_ALLOWED || '[]')
 logger(prefix, `accessList (RANGELIST_ALLOWED) for '/rangelist.txt' access`, accessRangelist)
 
-const ipAllowBlacklist = (ip: string)=>{
+const ipAllowBlacklist = (ip: string) => {
 	/* convert from `::ffff:192.0.0.1` => `192.0.0.1` */
-	if(ip.startsWith("::ffff:")){
-    ip = ip.substring(7)
+	if (ip.startsWith("::ffff:")) {
+		ip = ip.substring(7)
 	}
 	return accessBlacklist.includes(ip)
 }
-const ipAllowRangelist = (ip: string)=>{
-	if(ip.startsWith("::ffff:")){
-    ip = ip.substring(7)
+const ipAllowRangelist = (ip: string) => {
+	if (ip.startsWith("::ffff:")) {
+		ip = ip.substring(7)
 	}
 	return accessRangelist.includes(ip)
 }
 
 
 
-app.get('/', async(req, res)=> {
+app.get('/', async (req, res) => {
 	res.setHeader('Content-Type', 'text/plain')
 	res.write('Webserver operational.\n\n\n')
 
 	const ip = req.headers['x-forwarded-for'] as string || 'undefined'
 	res.write(`your ip is ${ip}\n`)
-	if(process.env.BLACKLIST_ALLOWED){
+	if (process.env.BLACKLIST_ALLOWED) {
 		res.write(`access blacklist: ${ipAllowBlacklist(ip)}\n`)
-	}else{
+	} else {
 		res.write('$BLACKLIST_ALLOWED is not defined\n')
 	}
-	if(process.env.RANGELIST_ALLOWED){
+	if (process.env.RANGELIST_ALLOWED) {
 		res.write(`access rangelist: ${ipAllowRangelist(ip)}\n`)
-	}else{
+	} else {
 		res.write('$RANGELIST_ALLOWED is not defined\n')
 	}
 	res.write('\n\n')
-	
+
 	const text = JSON.stringify(await si.osInfo())
 	res.write('\n\n\n' + text)
 
 	res.status(200).end()
 })
 
-app.get('/blacklist.txt', async(req, res)=> {
+app.get('/blacklist.txt', async (req, res) => {
 	/* if $BLACKLIST_ALLOWED not defined we let everyone access */
-	if(process.env.BLACKLIST_ALLOWED){
+	if (process.env.BLACKLIST_ALLOWED) {
 		const ip = req.headers['x-forwarded-for'] as string || 'undefined'
-		if(!ipAllowBlacklist(ip)){
+		if (!ipAllowBlacklist(ip)) {
 			logger('blacklist', `ip '${ip}' denied access`)
 			return res.status(403).send('403 Forbidden')
 		}
@@ -75,34 +75,34 @@ app.get('/blacklist.txt', async(req, res)=> {
 	res.status(200).end()
 })
 
-app.get('/rangelist.txt', async(req, res)=> {
+app.get('/rangelist.txt', async (req, res) => {
 	/* if $RANGELIST_ALLOWED not defined we let everyone access */
-	if(process.env.RANGELIST_ALLOWED){
+	if (process.env.RANGELIST_ALLOWED) {
 		const ip = req.headers['x-forwarded-for'] as string || 'undefined'
-		if(!ipAllowRangelist(ip)){
+		if (!ipAllowRangelist(ip)) {
 			logger('rangelist', `ip '${ip}' denied access`)
 			return res.status(403).send('403 Forbidden')
 		}
 		logger('rangelist', `ip '${ip}' access granted`)
 	}
-	
+
 	res.setHeader('Content-Type', 'text/plain')
 	await getRangelist(res)
 	res.status(200).end()
 })
 
-app.get('/nocache-stats.html', async(req, res)=> {
+app.get('/nocache-stats.html', async (req, res) => {
 	res.setHeader('Content-Type', 'text/html')
 	await getStatsTestOnly(res)
 	res.end()
 })
 
-app.get('/perf',async (req, res) => {
+app.get('/perf', async (req, res) => {
 	res.setHeader('Content-Type', 'text/html')
 	await getPerfHistory(res)
-	res.status(200).end() 
+	res.status(200).end()
 })
 
-app.listen(port, ()=> logger(`started on http://localhost:${port}`))
+app.listen(port, () => logger(`started on http://localhost:${port}`))
 
 
