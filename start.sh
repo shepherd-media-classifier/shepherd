ba493be95ec16edeac907e643b35dda77afa2d6e
Ros McMahon <rosmcmahon@gmail.com>
2022-11-30 09:04:47 +0700

    task: local fixes

--- start.sh
+++ start.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
 
 # exit on errors
-set -eo pipefail
+set -euo pipefail
+
+# for any relative paths
+export SCRIPT_DIR=$(dirname "$(realpath $0)")
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+
+function create_nsfw_config_file {
+	# check if `shepherd.config.json` exists, if not create default.
+	CONFIG_FILE="$SCRIPT_DIR/addons/nsfw/shepherd.config.json"
+	if [ ! -f "$CONFIG_FILE" ]; then
+		echo "$CONFIG_FILE not found. creating default..." 2>&1 | tee -a setup.log
+		### beginning of indentation mess after this line
+		cat <<EOF > "$CONFIG_FILE"
+{
+	"plugins": [ 
+		"shepherd-plugin-nsfw@latest"
+	],
+	"lowmem": false
+}
+EOF
+		### end of indentation mess
+	fi
+}
+
 
 # import .env vars
 if [ -f ".env.local" ]; then
 	export $(egrep -v '^#' .env.local | xargs)
 	# check vars
-	if [[ -z $EXTRA_QUEUES ]]; then
+	EXTRA_QUEUES_CHECKER=${EXTRA_QUEUES:-}
+	if [[ -z $EXTRA_QUEUES_CHECKER ]]; then
 		echo "INFO: EXTRA_QUEUES undefined"
 	else
 		echo "EXTRA_QUEUES=$EXTRA_QUEUES"
 	fi
 		
-	echo "PLUGIN=$PLUGIN"
+	PLUGIN_CHECKER=${PLUGIN:-}
+	if [[ -z $PLUGIN_CHECKER ]]; then
+		echo "INFO: PLUGIN undefined"
+		export PLUGIN=nsfw
+	else
+		echo "PLUGIN=$PLUGIN"
+	fi
+
+	create_nsfw_config_file
+		
 	echo "BLACKLIST_ALLOWED=$BLACKLIST_ALLOWED"
 	echo "RANGELIST_ALLOWED=$RANGELIST_ALLOWED"
 	echo "GW_URLS=$GW_URLS"
-	if [[ -z $PLUGIN || -z $EXTRA_QUEUES ]]; then
-		echo "INFO: some environment variables not set, using defaults. check .env.example"
-	fi
+
 
 	# make sure .env ends in newline
 	lastchar=$(tail -c 1 .env.local)
@@ -29,9 +60,7 @@ if [ -f ".env.local" ]; then
 else
 	echo "continuing without .env.local file."
 fi
-# for any relative paths
-export SCRIPT_DIR=$(dirname "$(realpath $0)")
-echo "SCRIPT_DIR=$SCRIPT_DIR"
+
 
 
 docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml up --build -d 
