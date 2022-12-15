f3315cadebd90f8cf15d832d437b44528effbf6a
Ros McMahon <rosmcmahon@gmail.com>
2022-12-15 01:52:21 +0000

    task: improve slack log for unresponsive nodes

--- services/src/webserver/txidToRange/fetch-retry.ts
+++ services/src/webserver/txidToRange/fetch-retry.ts
@@ -36,12 +36,12 @@ export const fetchRetryConnection = async(url: string)=> {
 		}catch(e:any){
 			connErrCount++
 			if(connErrCount > 30){
-				slackLogger(fetchRetryConnection.name, `Error for '${url}'. Already retried 30 times over 5m. Giving up.`)
-				console.log(fetchRetryConnection.name, `Error for '${url}'. Already retried 30 times over 5m. Giving up.`)
+				slackLogger(fetchRetryConnection.name, `Error for '${url}'. Already retried 30 times over 5m. Giving up. ${e.name}:${e.message}`)
+				console.log(fetchRetryConnection.name, `Error for '${url}'. Already retried 30 times over 5m. Giving up. ${e.name}:${e.message}`)
 				throw new Error(`${fetchRetryConnection.name} giving up after 30 retries. ${e.message}`)
 			}
 			//retry all of these connection errors
-			console.log(fetchRetryConnection.name, `Error for '${url}'. Retrying in ${retryMs} ms...`)
+			console.log(fetchRetryConnection.name, `Error for '${url}'. ${e.name}:${e.message}. Retrying in ${retryMs} ms...`)
 			console.log(e)
 			//clean up any stream resources
 			aborter?.abort()
