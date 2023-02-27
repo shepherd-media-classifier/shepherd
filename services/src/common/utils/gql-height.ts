0289f55eedf70024e4731fc676d1f5b0ae3802ab
Ros McMahon <rosmcmahon@gmail.com>
2023-02-27 16:26:04 +0700

    task: remove axios dep from `getGqlHeight`

--- services/src/common/utils/gql-height.ts
+++ services/src/common/utils/gql-height.ts
@@ -1,16 +1,28 @@
-import axios from "axios"
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-/* use GQL height over /info height, as both can get out of sync */
+/**  
+ * use GQL height over /info height, as both can get out of sync.
+ * retry on all errors.
+ * */
 export const getGqlHeight = async (endpoint: string) => {
 	while(true){
 		try{
 			const query = "query($minBlock: Int){ blocks( height:{min:$minBlock} first:1 ){ edges{node{height}} }}"
-			const { data } =  await axios.post(endpoint, {	query })
-			return +data.data.blocks.edges[0].node.height
+			const res = await fetch(endpoint, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ query })
+			})
+
+			if(!res.ok) throw Error(`${res.status} : ${res.statusText}`)
+
+			return (await res.json()).data.blocks.edges[0].node.height
 		}catch(e:any){
-			console.log('error polling gql height', e.message, 'retrying in 10s..')
+			console.log('error polling gql height', `"${e.message}"`, 'retrying in 10s..')
 			await sleep(10000)
 		}
 	}
