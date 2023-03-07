d1b0ad02b8461fd1b8ac3e267eea609d14abcda7
Ros McMahon <rosmcmahon@gmail.com>
2023-03-07 13:01:56 +0700

    fix/webserver: perf mon job running over itself in extreme backlog situation. noticeably degrading db perf

--- services/src/webserver/perf-cron.ts
+++ services/src/webserver/perf-cron.ts
@@ -7,32 +7,39 @@ const knex = dbConnection()
 
 const INTERVAL = 60*60*1000 //60 mins
 
+let _running = false
 const cronjob = async()=> {
-	// count group by flagged
-	const results = await knex<TxRecord>('txs').select('flagged').count('*').groupBy('flagged')
+	/** with very large backlog of records, a grouped flagged count query is taking 90+ minutes to complete.
+	 * (total txs 90m, backlog in the 10s of millions.)
+	 * replacing this with an estimate of total txs records, and index on `flagged is null`. the index will
+	 * still be slow when there is a large backlog as postgres won't use the index, but this arrangement 
+	 * should still be relatively quicker.
+	 */
+	if(_running) return;
+	_running = true
 
-	const output: Partial<HistoryRecord> = {}
-	let totalTxs = 0
-	for (const result of results) {
-		//@ts-ignore
-		const count: string = result.count
-		if(result.flagged === null){
-			output.unflagged = count
-		}
-		totalTxs += +count
+	/** removing this offending query */
+	// const results = await knex<TxRecord>('txs').select('flagged').count('*').groupBy('flagged')
+
+	/* inaccurate, but hopefully good enough */
+	const resTotalTxs = await knex('pg_class').select(knex.raw(`reltuples as estimate`)).where({ relname: 'txs' })
+	const totalTxs: number = resTotalTxs[0].estimate
+
+	/* costly query when `unflagged is null` count is high in backlog situations */
+	const resUnflagged = await knex('txs').count('*').whereNull('flagged')
+	const unflagged = resUnflagged[0]?.count || "0"
+
+	const output: HistoryRecord = {
+		total_txs: totalTxs.toString(),
+		unflagged: unflagged.toString(),
+		gql_height: await getGqlHeight(GQL_URL),
+		indexer_pass1: (await knex<StateRecord>('states').where({pname: 'indexer_pass1'}))[0].value
 	}
-
-	//fix crash when txs table empty
-	if(results.length === 0){
-		output.unflagged = "0"
-	}
-
-	output.total_txs = totalTxs.toString()
-	output.gql_height = await getGqlHeight(GQL_URL)
-	output.indexer_pass1 = (await knex<StateRecord>('states').where({pname: 'indexer_pass1'}))[0].value
 
 	console.log('[perf-cron]', output)
 	await knex<HistoryRecord>('history').insert(output)
+	
+	_running = false
 }
 
 setInterval(()=> {
