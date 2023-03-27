da7ebb695dc316494e8d8aeab91678af7277e939
Ros McMahon <rosmcmahon@gmail.com>
2023-03-27 22:53:53 +0000

    fix/nsfw: `file-type` wrong mime returned for SVGs

--- addons/nsfw/src/rating/image-filetype.ts
+++ addons/nsfw/src/rating/image-filetype.ts
@@ -4,5 +4,6 @@ import filetype from 'file-type'
 export const getImageMime = async(buffer: Uint8Array)=> {
 	const type = await filetype.fromBuffer(buffer)
 	if(type === undefined) return undefined
+	if(type.mime === 'application/xml') return 'image/svg+xml' //feature with the `file-type` library
 	return type.mime;
 }
