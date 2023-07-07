bbfc3c6546e5ea97d88f8d8ede049937ab06e8c8
Ros McMahon <rosmcmahon@gmail.com>
2023-07-07 03:28:01 +0000

    fix/http-api: extend `headersTimeout` + extra extensive logging

--- services/src/http-api/index.ts
+++ services/src/http-api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Socket } from 'net'
 import { logger } from '../common/shepherd-plugin-interfaces/logger'
 import { slackLogger } from '../common/utils/slackLogger'
 import { network_EXXX_codes } from '../common/constants'
@@ -47,18 +48,56 @@ app.post('/postupdate', async(req, res)=>{
 	}
 })
 
-export const server = app.listen(port, ()=> logger(`started on http://localhost:${port}`))
+export const server = app.listen(port, ()=> {
+	/** we're getting "clientError" 400 sent back to client. adjusting this timeout */
+	server.headersTimeout = 120_000 //default (nodejs) appears to be 60_000 currnently
 
-/** catch malformed client requests */
-server.on('clientError', (e: any, socket)=> {
+	logger(`started on http://localhost:${port}`)
+	//debug
+	console.log(`Server settings:`, 
+	{
+		timeout: server.timeout,
+		requestTimeout: server.requestTimeout, 
+		headersTimeout: server.headersTimeout,
+		keepAliveTimeout: server.keepAliveTimeout,
+		//@ts-ignore
+		connectionsCheckingInterval: server.connectionsCheckingInterval, allowHalfOpen: server.allowHalfOpen, pauseOnConnect: server.pauseOnConnect, 
+		//@ts-ignore
+		keepAlive: server.keepAlive, keepAliveInitialDelay: server.keepAliveInitialDelay, httpAllowHalfOpen: server.httpAllowHalfOpen,
+		maxHeadersCount: server.maxHeadersCount, maxRequestsPerSocket: server.maxRequestsPerSocket,
+	}
+	)
+})
+
+/** catch malformed client requests for example, might emit for server issues also though */
+server.on('clientError', (e: any, socket: Socket)=> {
 	logger(`express-clientError`, `${e.name} (${e.code}) : ${e.message}. socket.writable=${socket.writable} \n${e.stack}`)
+		//debug
+		console.log(`Socket:`, {
+			timeout: socket.timeout,
+			allowHalfOpen: socket.allowHalfOpen,
+			destroyed: socket.destroyed,
+			remoteAddress: socket.remoteAddress,
+			remoteFamily: socket.remoteFamily,
+			remotePort: socket.remotePort,
+			bytesRead: socket.bytesRead,
+			bytesWritten: socket.bytesWritten,
+			connecting: socket.connecting,
+			readyState: socket.readyState,
+			closed: socket.closed,
+			errored: socket.errored,
+			pending: socket.pending,
+			readable: socket.readable,
+			writable: socket.writable,
+		})
+
 	//make sure connection still open
 	if(
 		( e.code && network_EXXX_codes.includes(e.code) )
 		|| !socket.writable) {
     return;
   }
-	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n') //is this confusing? should we send a 500 sometimes?
 })
 
 server.on('error', (e: any)=> {
