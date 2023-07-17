38fc5b93f9dd135c09440b87c871bd5887bdb337
Ros McMahon <rosmcmahon@gmail.com>
2023-07-17 21:34:46 +0700

    feat: init commit. load multiple addon compose files using bash

--- start.sh
+++ start.sh
@@ -53,16 +53,36 @@ else
 	echo "continuing without .env.local file."
 fi
 
-# import ..RANGELIST_ALLOWED.json as a string
+#########################################################################
+# this should be merged with the previous `plugin` check once completed #
+#########################################################################
+
+
+# Example array of compose file paths
+plugin_dirs=("nsfw" "do-not-store")
+
+command_string="docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml "
+
+for dir in "${plugin_dirs[@]}"; do
+	echo $dir
+	command_string+="-f $SCRIPT_DIR/addons/$dir/docker-compose.local.yml "
+done
+echo $command_string
+
+
+
+
+# import `.RANGELIST_ALLOWED.json` as a string
 if [ ! -f ".RANGELIST_ALLOWED.json" ]; then
 	echo "WARNING: .RANGELIST_ALLOWED.json not found"
 else
 	export RANGELIST_ALLOWED=$(cat ./.RANGELIST_ALLOWED.json | tr -d ' \n\t')
+	echo "RANGELIST_ALLOWED=$RANGELIST_ALLOWED"
 fi
-echo "RANGELIST_ALLOWED=$RANGELIST_ALLOWED"
 
 
+# docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml up --build -d 
+eval "$command_string up --build -d"
 
-docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml up --build -d 
-
-docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml logs -f
+# docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml logs -f
+eval "$command_string logs -f"
