751629528f49b612849c0bcc1da4c0b996b2e670
Ros McMahon <rosmcmahon@gmail.com>
2023-07-20 15:43:09 +0700

    feat/multi-plugins: generate local compose files with addon service entries

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# -= boilerplate =-
+
 # exit on errors
 set -euo pipefail
 
@@ -7,82 +9,109 @@ set -euo pipefail
 export SCRIPT_DIR=$(dirname "$(realpath $0)")
 echo "SCRIPT_DIR=$SCRIPT_DIR"
 
-# check if `shepherd.config.json` exists, if not create default.
+
+# -= ensure `shepherd.config.json` exists =-
+
 CONFIG_FILE="$SCRIPT_DIR/addons/nsfw/shepherd.config.json"
 if [ ! -f "$CONFIG_FILE" ]; then
-	echo "$CONFIG_FILE not found. creating default..." 2>&1 | tee -a setup.log
-	cat <<EOF > "$CONFIG_FILE"
+  echo "$CONFIG_FILE not found. creating default..." 2>&1 | tee -a setup.log
+  cat <<EOF > "$CONFIG_FILE"
 {
 "plugins": [ 
-	"shepherd-plugin-nsfw@latest"
+  "shepherd-plugin-nsfw@latest"
 ],
 "lowmem": false
 }
 EOF
 fi
 
-# import .env vars
+# -= import .env vars =-
+
 if [ -f ".env.local" ]; then
-	export $(egrep -v '^#' .env.local | xargs)
-	# check vars
-	EXTRA_QUEUES_CHECKER=${EXTRA_QUEUES:-}
-	if [[ -z $EXTRA_QUEUES_CHECKER ]]; then
-		echo "INFO: EXTRA_QUEUES undefined"
-	else
-		echo "EXTRA_QUEUES=$EXTRA_QUEUES"
-	fi
-		
-	PLUGIN_CHECKER=${PLUGIN:-}
-	if [[ -z $PLUGIN_CHECKER ]]; then
-		echo "INFO: PLUGIN undefined"
-		export PLUGIN=nsfw
-	else
-		echo "PLUGIN=$PLUGIN"
-	fi
-		
-	echo "BLACKLIST_ALLOWED=$BLACKLIST_ALLOWED"
-	echo "GW_URLS=$GW_URLS"
+  export $(egrep -v '^#' .env.local | xargs)
+  # check vars
+  extra_queues_checker=${EXTRA_QUEUES:-}
+  if [[ -z $extra_queues_checker ]]; then
+    echo "INFO: EXTRA_QUEUES undefined"
+  else
+    echo "EXTRA_QUEUES=$EXTRA_QUEUES"
+  fi
+    
+  plugin_checker=${PLUGIN:-}
+  if [[ -z $plugin_checker ]]; then
+    echo "INFO: PLUGIN undefined"
+    export PLUGIN=nsfw
+  else
+    echo "PLUGIN=$PLUGIN"
+  fi
+    
+  echo "BLACKLIST_ALLOWED=$BLACKLIST_ALLOWED"
+  echo "GW_URLS=$GW_URLS"
 
 
-	# make sure .env ends in newline
-	lastchar=$(tail -c 1 .env.local)
-	if [ "$lastchar" != "" ]; then 
-		echo >> .env
-	fi
+  # make sure .env ends in newline
+  lastchar=$(tail -c 1 .env.local)
+  if [ "$lastchar" != "" ]; then 
+    echo >> .env
+  fi
 else
-	echo "continuing without .env.local file."
+  echo "continuing without .env.local file."
 fi
-
-#########################################################################
-# this should be merged with the previous `plugin` check once completed #
-#########################################################################
-
-
-# Example array of compose file paths
-plugin_dirs=("nsfw" "do-not-store")
-
-command_string="docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml "
-
-for dir in "${plugin_dirs[@]}"; do
-	echo $dir
-	command_string+="-f $SCRIPT_DIR/addons/$dir/docker-compose.local.yml "
-done
-echo $command_string
-
-
 
 
 # import `.RANGELIST_ALLOWED.json` as a string
+
 if [ ! -f ".RANGELIST_ALLOWED.json" ]; then
-	echo "WARNING: .RANGELIST_ALLOWED.json not found"
+  echo "WARNING: .RANGELIST_ALLOWED.json not found"
 else
-	export RANGELIST_ALLOWED=$(cat ./.RANGELIST_ALLOWED.json | tr -d ' \n\t')
-	echo "RANGELIST_ALLOWED=$RANGELIST_ALLOWED"
+  export RANGELIST_ALLOWED=$(cat ./.RANGELIST_ALLOWED.json | tr -d ' \n\t')
+  echo "RANGELIST_ALLOWED=$RANGELIST_ALLOWED"
+fi
+
+##########################################
+# -= generate compose file to be used =- #
+##########################################
+
+# copy override file to generated file
+generated_file="compose.generated.yml"
+cat "$SCRIPT_DIR/docker-compose.override.yml" > $generated_file
+
+# function to generate the sevice entry for a plugin
+function generate_service_entry {
+  local plugin_name=$1
+  cat << EOF >> $generated_file
+
+  # automatically generated service entry
+  $plugin_name:
+    extends:
+      file: ./addons/$plugin_name/docker-compose.local.yml
+      service: $plugin_name
+EOF
+}
+
+# import PLUGINS and 
+plugins_checker=${PLUGINS:-}
+if [[ -z $plugins_checker ]]; then
+  echo "INFO: PLUGINS=undefined." #TODO: add default behaviour here
+else
+  echo "PLUGINS=$PLUGINS"
+  IFS=',' read -r -a plugin_dirs <<< "$PLUGINS" # plugins_dirs = PLUGINS.split(',')
+  for plugin_name in "${plugin_dirs[@]}"; do
+    echo "generating service entry for $plugin_name"
+    generate_service_entry $plugin_name
+  done
 fi
 
 
+
+##########################################
+# -= finally run the compose commands =- #
+##########################################
+
+command_string="docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/$generated_file "
+
 # docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml up --build -d 
-eval "$command_string up --build -d"
+eval "$command_string up --build "
 
 # docker compose -f $SCRIPT_DIR/docker-compose.yml -f $SCRIPT_DIR/docker-compose.override.yml logs -f
 eval "$command_string logs -f"
