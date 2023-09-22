bb5852aa63a843107122aacacef90de07d3b179e
Ros McMahon <rosmcmahon@gmail.com>
2023-09-22 14:31:43 +0000

    fix/http-api: error result object not printing

--- services/src/http-api/pluginResultHandler.ts
+++ services/src/http-api/pluginResultHandler.ts
@@ -1,10 +1,10 @@
-import { getByteRange } from "../byte-ranges/byteRanges"
-import { APIFilterResult } from "../common/shepherd-plugin-interfaces"
-import { logger } from "../common/shepherd-plugin-interfaces/logger"
-import { dbCorruptDataConfirmed, dbCorruptDataMaybe, dbInflightDel, dbOversizedPngFound, dbPartialImageFound, dbUnsupportedMimeType, dbWrongMimeType, getTxFromInbox, updateInboxDb } from "../common/utils/db-update-txs"
-import { slackLogger } from "../common/utils/slackLogger"
-import { slackLoggerPositive } from "../common/utils/slackLoggerPositive"
-import { moveInboxToTxs } from "./move-records"
+import { getByteRange } from '../byte-ranges/byteRanges'
+import { APIFilterResult } from '../common/shepherd-plugin-interfaces'
+import { logger } from '../common/shepherd-plugin-interfaces/logger'
+import { dbCorruptDataConfirmed, dbCorruptDataMaybe, dbInflightDel, dbOversizedPngFound, dbPartialImageFound, dbUnsupportedMimeType, dbWrongMimeType, getTxFromInbox, updateInboxDb } from '../common/utils/db-update-txs'
+import { slackLogger } from '../common/utils/slackLogger'
+import { slackLoggerPositive } from '../common/utils/slackLoggerPositive'
+import { moveInboxToTxs } from './move-records'
 import { doneAdd } from './done-records'
 
 
@@ -13,12 +13,12 @@ let count = 0
 export const pluginResultHandler = async(body: APIFilterResult)=>{
 	const txid = body.txid
 	const result = body.filterResult
-	
+
 	const c = ++count
 	logger(txid, `handler begins. count ${c}`)
 
 	if((typeof txid !== 'string') || txid.length !== 43){
-		logger(`Fatal error`,`txid is not defined correctly: ${body?.txid}`)
+		logger('Fatal error',`txid is not defined correctly: ${body?.txid}`)
 		throw new TypeError('txid is not defined correctly')
 	}
 
@@ -33,34 +33,35 @@ export const pluginResultHandler = async(body: APIFilterResult)=>{
 				slackLogger('✅ *Test Message* ✅', JSON.stringify(body))
 			}
 
-			let byteStart, byteEnd;
+			let byteStart, byteEnd
 			if(
 				Number(result.top_score_value) > 0.9
 				|| result.flagged === true
 			){
-				try {
+				try{
 					/** get the tx data from the database */
 					const record = await getTxFromInbox(txid)
 
 					/** sqs messages can be read more than once */
 					if(!record){
-						logger(txid, pluginResultHandler.name, `record not found in inbox`)
-						slackLogger(txid, pluginResultHandler.name, `record not found in inbox`, result)
-						return;
+						logger(txid, pluginResultHandler.name, 'record not found in inbox', JSON.stringify(result))
+						slackLogger(txid, pluginResultHandler.name, 'record not found in inbox', JSON.stringify(result))
+						return
 					}
-	
+
 					/** calculate the byte range */
 					const { start, end } = await getByteRange(txid, record.parent, record.parents)
 					byteStart = start.toString()
 					byteEnd = end.toString()
-	
+
 					console.log(txid, `calculated byte-range ${byteStart} to ${byteEnd}`)
-				} catch (e:any) {
+				}catch(err:unknown){
+					const e = err as Error
 					logger(txid, `Error calculating byte-range: ${e.name}:${e.message}`, JSON.stringify(e))
 					slackLogger(txid, pluginResultHandler.name, `Error calculating byte-range: ${e.name}:${e.message}`, JSON.stringify(e))
 				}
 			}
-			
+
 			const res = await updateInboxDb(txid, {
 				flagged: result.flagged,
 				valid_data: true,
@@ -72,7 +73,7 @@ export const pluginResultHandler = async(body: APIFilterResult)=>{
 			})
 
 			if(res !== txid){
-				logger(`Fatal error`, `Could not update database. "${res} !== ${txid}"`)
+				logger('Fatal error', `Could not update database. "${res} !== ${txid}"`)
 				throw new Error('Could not update database')
 			}
 
@@ -84,51 +85,51 @@ export const pluginResultHandler = async(body: APIFilterResult)=>{
 						slackLogger(`${txid}. ${moveInboxToTxs.name} returned '${moved}' not '1'`)
 						throw new Error(`${txid}. ${moveInboxToTxs.name} returned '${moved}' not '1'`)
 					}
-					return;
+					return
 				}catch(e){
-					logger(txid, `Error moving flagged record from inbox to txs`, JSON.stringify(e))
-					slackLogger(txid, `Error moving flagged record from inbox to txs`, JSON.stringify(e))
+					logger(txid, 'Error moving flagged record from inbox to txs', JSON.stringify(e))
+					slackLogger(txid, 'Error moving flagged record from inbox to txs', JSON.stringify(e))
 				}
 			}else{
 				await doneAddTested(txid)
 			}
-			
+
 		}else if(result.data_reason === undefined){
 			logger(txid, 'data_reason and flagged cannot both be undefined. deleting from inflights.')
 			await dbInflightDel(txid)
 			throw new TypeError('data_reason and flagged cannot both be undefined')
 		}else{
-			switch (result.data_reason) {
-				case 'corrupt-maybe':
-					await dbCorruptDataMaybe(txid)
-					break;
-				case 'corrupt':
-					await dbCorruptDataConfirmed(txid)
-					break;
-				case 'oversized':
-					await dbOversizedPngFound(txid)
-					break;
-				case 'partial':
-					await dbPartialImageFound(txid)
-					break;
-				case 'unsupported':
-					await dbUnsupportedMimeType(txid)
-					break;
-				case 'mimetype':
-					await dbWrongMimeType(txid, result.err_message!)
-					break;
-				case 'retry':
-					await dbInflightDel(txid) //this is all we actually want done
-					return;
-			
-				default:
-					logger(pluginResultHandler.name, 'UNHANDLED plugin result in http-api', txid)
-					slackLogger(pluginResultHandler.name, 'UNHANDLED plugin result in http-api', txid)
-					throw new Error('UNHANDLED plugin result in http-api:\n' + JSON.stringify(result))
+			switch (result.data_reason){
+			case 'corrupt-maybe':
+				await dbCorruptDataMaybe(txid)
+				break
+			case 'corrupt':
+				await dbCorruptDataConfirmed(txid)
+				break
+			case 'oversized':
+				await dbOversizedPngFound(txid)
+				break
+			case 'partial':
+				await dbPartialImageFound(txid)
+				break
+			case 'unsupported':
+				await dbUnsupportedMimeType(txid)
+				break
+			case 'mimetype':
+				await dbWrongMimeType(txid, result.err_message!)
+				break
+			case 'retry':
+				await dbInflightDel(txid) //this is all we actually want done
+				return
+
+			default:
+				logger(pluginResultHandler.name, 'UNHANDLED plugin result in http-api', txid)
+				slackLogger(pluginResultHandler.name, 'UNHANDLED plugin result in http-api', txid)
+				throw new Error('UNHANDLED plugin result in http-api:\n' + JSON.stringify(result))
 			}
 			await doneAddTested(txid)
 		}
-	}finally{
+	} finally {
 		// await dbInflightDel(txid)
 		logger(txid, `handler finished. count ${c}`)
 	}
@@ -138,11 +139,10 @@ const doneAddTested = async(txid: string)=> {
 	const record = await getTxFromInbox(txid)
 	if(record){
 		if(record.flagged !== undefined || record.valid_data !== undefined){
-			logger(txid, `flagged or valid_data set. calling doneAdd`)
+			logger(txid, 'flagged or valid_data set. calling doneAdd')
 			await doneAdd(txid, record.height)
 		}else{
-			logger(txid, `flagged or valid_data not set.`)
+			logger(txid, 'flagged or valid_data not set.')
 		}
 	}
 }
- 
