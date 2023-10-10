fc9b181ca665246e317552615facda882a099fa1
Ros McMahon <rosmcmahon@gmail.com>
2023-10-10 17:06:40 +0700

    task/nsfw: update images to slim + fix some tests

--- addons/nsfw/node18-ffmpeg/build.sh
+++ addons/nsfw/node18-ffmpeg/build.sh
@@ -3,5 +3,5 @@ echo 'Warning this will change your docker build system'
 docker buildx install
 docker buildx create --use --name shepbuild
 docker login
-docker buildx build --push --tag rosmcmahon/node18-ffmpeg:multi-arch --platform linux/amd64,linux/arm64 . 
+docker buildx build --push --tag rosmcmahon/node18slim-ffmpeg:multi-arch --platform linux/amd64,linux/arm64 . 
 docker buildx rm shepbuild
