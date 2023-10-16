4dc2bda405d8f367f67ca129793c72e3ada79f61
Ros McMahon <rosmcmahon@gmail.com>
2023-10-16 17:31:26 +0700

    feat/services: provide external access via tailscale vpn service

--- services-aws/lib/services-stack.ts
+++ services-aws/lib/services-stack.ts
@@ -17,6 +17,7 @@ const envVarNames = [
 	'LB_DNSNAME',
 	'ShepherdPgdbSg',
 	'ShepherdAlbSg',
+	'TS_AUTHKEY',
 ]
 envVarNames.map(name => {
 	if (!process.env[name]) throw new Error(`${name} not set`)
@@ -52,7 +53,7 @@ export class ServicesStack extends cdk.Stack {
 
 		const fgNginx = fargateNginx({ stack, cluster, logGroup, vpc, alb, sgAlb, port: 80 })
 
-		// const tailscale = createTailscale({ stack, cluster, logGroup, alb, port: 443 }, vpc)
+		const tailscale = createTailscale({ stack, cluster, logGroup, vpc, alb, sgAlb, port: 443 })
 
 
 
@@ -75,19 +76,6 @@ interface FargateBuilderIngressProps extends FargateBuilderProps {
 
 const fargateNginx = ({ stack, cluster, logGroup, vpc, alb, port, sgAlb }: FargateBuilderIngressProps) => {
 
-	/** create security group for nginx fargate */
-
-	// const sgNginx = new cdk.aws_ec2.SecurityGroup(stack, 'sgNginx', {
-	// 	vpc,
-	// 	securityGroupName: 'sgNginx',
-	// 	allowAllOutbound: true,
-	// })
-	// sgNginx.addIngressRule(sgAlb, cdk.aws_ec2.Port.tcp(port), 'allow traffic from alb')
-
-	// /** ALB SG rules */
-	// sgAlb.addEgressRule(sgNginx, cdk.aws_ec2.Port.allTraffic(), 'allow all traffic from sgNginx out to the internet')
-	// sgAlb.addIngressRule(cdk.aws_ec2.Peer.anyIpv4(), cdk.aws_ec2.Port.tcp(port), `allow traffic from the internet on port ${port}`)
-
 	/** create tdef, image, service */
 
 	const taskDefinition = new cdk.aws_ecs.FargateTaskDefinition(stack, 'tdefNginx', {
@@ -108,7 +96,6 @@ const fargateNginx = ({ stack, cluster, logGroup, vpc, alb, port, sgAlb }: Farga
 		// securityGroups: [sgNginx]
 	})
 
-
 	/** add port mapping */
 
 	alb.addListener('fargateFromWebListener', {
@@ -120,52 +107,35 @@ const fargateNginx = ({ stack, cluster, logGroup, vpc, alb, port, sgAlb }: Farga
 		targets: [fgNginx],
 	})
 
-
-
-	// alb.connections.allowFromAnyIpv4(cdk.aws_ec2.Port.tcp(port), `allow traffic from the internet on port ${port}`)
-	// fgNginx.connections.allowFrom(alb, cdk.aws_ec2.Port.tcp(port), `allow traffic from alb on port ${port}`)
-
-
 	return fgNginx
 }
 
-const createTailscale = ({ stack, cluster, logGroup, alb, port }: FargateBuilderIngressProps, vpc: cdk.aws_ec2.IVpc) => {
+const createTailscale = ({ stack, cluster, logGroup, vpc, alb, sgAlb, port }: FargateBuilderIngressProps) => {
+
+	/* N.B. there's no need for any AWS port mapping when using tailscale */
+
 	const tdefTailscale = new cdk.aws_ecs.FargateTaskDefinition(stack, 'tdefTailscale', {
 		cpu: 256,
 		memoryLimitMiB: 512,
 	})
 	tdefTailscale.addContainer('tailscaleImage', {
 		image: cdk.aws_ecs.ContainerImage.fromRegistry('tailscale/tailscale'),
-		portMappings: [{ containerPort: port }],
 		logging: new cdk.aws_ecs.AwsLogDriver({
 			logGroup,
 			streamPrefix: 'tailscale',
 		}),
 		containerName: 'tailscaleContainer',
 		environment: {
-			TS_AUTHKEY: 'tskey-auth-kHTacr3CNTRL-AsjAEnTwP36NHfbre3gV36YhdQ6UoGtM2',
-			TS_ROUTES: vpc.privateSubnets[0].ipv4CidrBlock, //.map(s => s.ipv4CidrBlock).join(','),
+			TS_AUTHKEY: process.env.TS_AUTHKEY!,
+			TS_ROUTES: vpc.privateSubnets.map(s => s.ipv4CidrBlock).join(','),
+			TS_EXPIRY_KEY_DISABLE: 'true',
+			TS_ADVERTISE_ROUTES: 'true',
 		}
 	})
 	const fgTailscale = new cdk.aws_ecs.FargateService(stack, 'fgTailscale', {
 		cluster,
 		taskDefinition: tdefTailscale,
-		// desiredCount: 1,
-		// minHealthyPercent: 100,
-		// maxHealthyPercent: 200,
 	})
-	alb
-
-	// alb.addListener('tailscaleListener', {
-	// 	protocol: cdk.aws_elasticloadbalancingv2.ApplicationProtocol.HTTP,
-	// 	port,
-	// 	// certificates: [cdk.aws_certificatemanager.Certificate.fromCertificateArn(stack, 'cert', 'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012')],
-	// })
-	// 	.addTargets('tailscaleTarget', {
-	// 		protocol: cdk.aws_elasticloadbalancingv2.ApplicationProtocol.HTTP,
-	// 		port,
-	// 		targets: [fgTailscale],
-	// 	})
 
 	return fgTailscale
 }
