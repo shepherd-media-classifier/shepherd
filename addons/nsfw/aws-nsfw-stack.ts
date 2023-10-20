b00c14944339eee6e226b85974fc2dbd20f83449
Ros McMahon <rosmcmahon@gmail.com>
2023-10-20 11:29:39 +0700

    feat/nsfw: check mandatory envs

--- addons/nsfw/aws-nsfw-stack.ts
+++ addons/nsfw/aws-nsfw-stack.ts
@@ -1,5 +1,17 @@
 import { App, Stack } from 'aws-cdk-lib'
 
+/** check mandatoy envs exist */
+const envs = [
+	'DB_HOST',
+	'AWS_SQS_INPUT_QUEUE',
+	'AWS_INPUT_BUCKET',
+	'AWS_DEFAULT_REGION',
+	'HTTP_API_URL',
+]
+envs.map((name: string) => {
+	if (!process.env[name]) throw new Error(`${name} not set`)
+})
+
 /** standard stack boilerplate */
 const app = new App()
 const stack = new Stack(app, 'NsfwStack', {
