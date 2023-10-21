07785d0b38d070c2eba4c61b4f7d71ba27a37873
Ros McMahon <rosmcmahon@gmail.com>
2023-10-21 19:55:14 +0700

    task/services: initial project conversion to esm + knex esm

--- services/knexfile.ts
+++ services/knexfile.ts
@@ -1,19 +1,19 @@
 // Update with your config settings.
 
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: 5432,
-    database: 'arblacklist',
-    user:     'postgres',
-    password: 'postgres'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+export default {
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		port: 5432,
+		database: 'arblacklist',
+		user:     'postgres',
+		password: 'postgres'
+	},
+	pool: {
+		min: 0,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
 }
