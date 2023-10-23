5470a1d9636531cd630aaafd6d92b33ffd8f592c
Ros McMahon <rosmcmahon@gmail.com>
2023-10-23 20:45:52 +0700

    fix/tests: cleanup docker container

--- test-grep.sh
+++ test-grep.sh
@@ -8,4 +8,4 @@ docker compose -p shepherd-test -f docker-compose.test.yml up -d s3-local && (do
 docker compose -p shepherd-test -f docker-compose.test.yml run test npm run test:grep "$1"
 # docker compose -p shepherd-test -f docker-compose.test.yml stop pgdb-test s3-local sqs-local
 # docker compose -p shepherd-test -f docker-compose.test.yml rm -f
-docker compose -p shepherd-test -f docker-compose.test.yml down
+docker compose -p shepherd-test -f docker-compose.test.yml down --remove-orphans
