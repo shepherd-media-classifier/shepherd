c3f82a57e3ec28000a46d28b9ae2351b6bf97563
Ros McMahon <rosmcmahon@gmail.com>
2023-10-24 08:55:00 +0700

    task/services: various fixes and improvements

--- test-grep.sh
+++ test-grep.sh
@@ -3,9 +3,9 @@ echo "$1"
 docker compose -p shepherd-test -f docker-compose.test.yml rm -f
 docker compose -p shepherd-test -f docker-compose.test.yml build
 docker compose -p shepherd-test -f docker-compose.test.yml up -d pgdb-test && sleep 1 # pgdb taking longer to be ready
-docker compose -p shepherd-test -f docker-compose.test.yml run test knex migrate:latest
+docker compose -p shepherd-test -f docker-compose.test.yml run test npm run migrate
 docker compose -p shepherd-test -f docker-compose.test.yml up -d s3-local && (docker compose logs -f s3-local &)
 docker compose -p shepherd-test -f docker-compose.test.yml run test npm run test:grep "$1"
 # docker compose -p shepherd-test -f docker-compose.test.yml stop pgdb-test s3-local sqs-local
 # docker compose -p shepherd-test -f docker-compose.test.yml rm -f
-docker compose -p shepherd-test -f docker-compose.test.yml down
+docker compose -p shepherd-test -f docker-compose.test.yml down --remove-orphans
--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 
 docker compose -p shepherd-test -f docker-compose.test.yml build
 docker compose -p shepherd-test -f docker-compose.test.yml up -d pgdb-test && sleep 1 # pgdb taking longer to be ready
-docker compose -p shepherd-test -f docker-compose.test.yml run test knex migrate:latest
+docker compose -p shepherd-test -f docker-compose.test.yml run test npm run migrate
 docker compose -p shepherd-test -f docker-compose.test.yml run test npm test
 docker compose -p shepherd-test -f docker-compose.test.yml stop pgdb-test
-docker compose -p shepherd-test -f docker-compose.test.yml rm -f
+docker compose -p shepherd-test -f docker-compose.test.yml down --remove-orphans
