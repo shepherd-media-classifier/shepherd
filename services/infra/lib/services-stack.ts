3d4113f4384f3092419ea66a2d9f06766665255b
Ros McMahon <rosmcmahon@gmail.com>
2023-11-20 12:42:22 +0700

    task/services: refactor. move cdk code into services/infra

--- services/infra/lib/services-stack.ts
+++ services/infra/lib/services-stack.ts
@@ -210,23 +210,23 @@ const createTailscale = (vpc: cdk.aws_ec2.IVpc, { stack, cluster, logGroup, }: F
 
 interface ServiceResources {
 	// this is handy for hovering over the props in vscode
-	/** Valid values for cpu and ram units used by the Fargate launch type. 
-	 * https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html 
-	 * 
+	/** Valid values for cpu and ram units used by the Fargate launch type.
+	 * https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
+	 *
 	 * 256 (.25 vCPU) - Available memory values: 512 (0.5 GB), 1024 (1 GB), 2048 (2 GB)
-	 * 
+	 *
 	 * 512 (.5 vCPU) - Available memory values: 1024 (1 GB), 2048 (2 GB), 3072 (3 GB), 4096 (4 GB)
-	 * 
+	 *
 	 * 1024 (1 vCPU) - Available memory values: 2048 (2 GB), 3072 (3 GB), 4096 (4 GB), 5120 (5 GB), 6144 (6 GB), 7168 (7 GB), 8192 (8 GB)
-	 * 
+	 *
 	 * 2048 (2 vCPU) - Available memory values: Between 4096 (4 GB) and 16384 (16 GB) in increments of 1024 (1 GB)
-	 *  
+	 *
 	 * 4096 (4 vCPU) - Available memory values: Between 8192 (8 GB) and 30720 (30 GB) in increments of 1024 (1 GB)
-	 * 
+	 *
 	 * 8192 (8 vCPU) - Available memory values: Between 16384 (16 GB) and 61440 (60 GB) in increments of 4096 (4 GB)
-	 * 
+	 *
 	 * 16384 (16 vCPU) - Available memory values: Between 32768 (32 GB) and 122880 (120 GB) in increments of 8192 (8 GB)
-	 * 
+	 *
 	 * @default cpu 256 && memoryLimitMiB: 512
 	 */
 	cpu: number
