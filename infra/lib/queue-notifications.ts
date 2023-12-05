879540d572d7feda8bb9187f5d2bad27817359ac
Ros McMahon <rosmcmahon@gmail.com>
2023-12-05 19:09:15 +0700

    feat/infra: alarm+topic for inputAgeMetric

--- infra/lib/queue-notifications.ts
+++ infra/lib/queue-notifications.ts
@@ -1,4 +1,4 @@
-import { Stack, aws_cloudwatch, aws_ec2 } from 'aws-cdk-lib'
+import { Duration, Stack, aws_cloudwatch, aws_cloudwatch_actions, aws_ec2, aws_sns } from 'aws-cdk-lib'
 
 export const inputQMetricAndNotifications = (
 	stack: Stack,
@@ -16,6 +16,20 @@ export const inputQMetricAndNotifications = (
 		},
 	}
 	const unexportableMetric = new aws_cloudwatch.Metric(inputAgeMetricProps)
+
+	/** create alarm */
+	const inputAgeAlarm = new aws_cloudwatch.Alarm(stack, 'inputAgeAlarm', {
+		metric: unexportableMetric,
+		threshold: 300,
+		evaluationPeriods: 1,
+		alarmDescription: 'shepherd2-input-q a message age is too old',
+		alarmName: 'shepherdInputAgeAlarm',
+		actionsEnabled: true,
+	})
+	const topic = new aws_sns.Topic(stack, 'inputAgeAlarmTopic', {
+		displayName: 'shepherd2-input-q a message age is too old',
+	})
+
 
 	return {
 		inputAgeMetricProps,
