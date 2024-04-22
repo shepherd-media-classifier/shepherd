eef60a31c1b6486e64971be2e13285cbcdc44410
Ros McMahon <rosmcmahon@gmail.com>
2024-04-22 20:01:41 +0700

    fix/feeder: extra logging in legacy record fixer + more retries

--- services/src/feeder/legacyFix-RecordOwner.ts
+++ services/src/feeder/legacyFix-RecordOwner.ts
@@ -1,6 +1,6 @@
 import { arGql } from 'ar-gql'
 
-const gqlPrimary = arGql(process.env.GQL_URL!) //fallback
+const gqlPrimary = arGql(process.env.GQL_URL!, 1) //fallback
 const gqlSecondary = arGql(process.env.GQL_URL_SECONDARY!, 1) //use this as default
 
 export const legacyRecordOwnerFix = async (txid: string) => {
@@ -9,6 +9,7 @@ export const legacyRecordOwnerFix = async (txid: string) => {
 		const meta = await gqlSecondary.tx(txid)
 		return meta.owner.address
 	}catch(error){
+		console.debug('legacyRecordOwnerFix fallback will be called due to previous', error)
 		const meta = await gqlPrimary.tx(txid)
 		return meta.owner.address
 	}
