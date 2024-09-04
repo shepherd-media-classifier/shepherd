7c87a0a1e03490ed4d66a48e11f7f916f5850c46
Ros McMahon <rosmcmahon@gmail.com>
2024-09-05 00:38:56 +0700

    fix(feeder): log txid on legacy owner errors

--- services/src/feeder/legacyFix-RecordOwner.ts
+++ services/src/feeder/legacyFix-RecordOwner.ts
@@ -4,12 +4,12 @@ const gqlPrimary = arGql(process.env.GQL_URL!, 1) //fallback
 const gqlSecondary = arGql(process.env.GQL_URL_SECONDARY!, 1) //use this as default
 
 export const legacyRecordOwnerFix = async (txid: string) => {
-	console.debug('legacyRecordOwnerFix got called', txid)
+	console.info('legacyRecordOwnerFix got called', txid)
 	try{
 		const meta = await gqlSecondary.tx(txid)
 		return meta.owner.address
 	}catch(error){
-		console.debug('legacyRecordOwnerFix fallback will be called due to previous', error)
+		console.info('legacyRecordOwnerFix fallback will be called due to previous', txid, error)
 		const meta = await gqlPrimary.tx(txid)
 		return meta.owner.address
 	}
