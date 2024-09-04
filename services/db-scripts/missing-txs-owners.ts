2b3966c5ba3b89c39c8ad8e3ed4cd67a8d028822
Ros McMahon <rosmcmahon@gmail.com>
2024-09-04 17:33:44 +0000

    fix(db-scripts): retry gql errors

--- services/db-scripts/missing-txs-owners.ts
+++ services/db-scripts/missing-txs-owners.ts
@@ -1,6 +1,7 @@
 import knexCreate from '../src/common/utils/db-connection'
 import { arGql, GQLUrls } from 'ar-gql'
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 const knex = knexCreate()
 const gql = arGql(GQLUrls.goldsky)
 
@@ -24,24 +25,33 @@ const main = async()=>{
 	/** get inbox txids */
 	const txids = await knex('txs').select('txid')
 		.where('top_score_value', '>=', 0.9)
-		.where('flagged', false)
+		// .where('flagged', true)
 		.whereNull('owner')
-	console.info(`${txids.length} inbox txids`)
+	console.info(`${txids.length} txids`)
 
 	while(txids.length > 0){
 		const ids = txids.splice(0, 100).map((txid)=>txid.txid)
 
-		const { data } = await gql.run(query, { ids } )
-		const edges = data.transactions.edges
+		while(true){
+			try{
+				const { data } = await gql.run(query, { ids } )
+				const edges = data.transactions.edges
+				console.debug('gql edges', edges.length) //need to switch provider?
 
-		await Promise.all(	edges.map(async (edge) => {
-			const owner = edge.node.owner.address
-			const txid = edge.node.id
-			console.log(
-				'updating',{txid, owner},
-				'result', await knex('txs').where({ txid }).update({ owner }).onConflict().ignore()
-			)
-		}))
+				await Promise.all(	edges.map(async (edge) => {
+					const owner = edge.node.owner.address
+					const txid = edge.node.id
+					console.log(
+						'updating',JSON.stringify({txid, owner}),
+						'result', await knex('txs').where({ txid }).update({ owner }).onConflict().ignore()
+					)
+				}))
+				break
+			}catch(e){
+				console.error('some error happened, dont care', e)
+				await sleep(5_000)
+			}
+		}
 	}
 
 
